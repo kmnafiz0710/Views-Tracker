9ba344d1cf296a3f051589d1e6233a60c18e74f6
Khaled Masud <155144168+kmnafiz0710@users.noreply.github.com>
2025-01-01 11:56:07 +0600

    Updated

--- custom-view-tracker.php
+++ custom-view-tracker.php
@@ -16,13 +16,3 @@ if (!defined('ABSPATH')) {
 require_once plugin_dir_path(__FILE__) . 'includes/tracker-functions.php';
 require_once plugin_dir_path(__FILE__) . 'includes/shortcode-functions.php';
 require_once plugin_dir_path(__FILE__) . 'includes/admin-functions.php';
-include_once plugin_dir_path(__FILE__) . 'includes/functions.php';
-include_once plugin_dir_path(__FILE__) . 'includes/widgets.php';
-include_once plugin_dir_path(__FILE__) . 'includes/blocks.php';
-include_once plugin_dir_path(__FILE__) . 'includes/elementor-widget.php';
-
-// Enqueue styles and scripts for Gutenberg and Elementor blocks
-function enqueue_plugin_scripts() {
-    wp_enqueue_script('view-tracker-block', plugins_url('js/view-tracker-block.js', __FILE__), ['wp-blocks', 'wp-element', 'wp-editor'], filemtime(plugin_dir_path(__FILE__) . 'js/view-tracker-block.js'));
-}
-add_action('enqueue_block_editor_assets', 'enqueue_plugin_scripts');
